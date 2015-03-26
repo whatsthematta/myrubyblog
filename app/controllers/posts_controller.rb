61db473ac302ffb032a08a387fbc57c45feeeb0a
Jason Matta <jasonmattaa@gmail.com>
2015-03-25 20:55:56 -0400

    heroku db update

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,30 @@
+class PostsController < ApplicationController
+	def index 
+		@content_first = "This is some sample text for our blog";
+		@content_second = "This is some sample text for our blog";
+	end
+
+	def new
+
+	end
+
+	def create
+
+	end
+
+	def edit
+
+	end
+
+	def update
+
+	end
+
+	def show
+
+	end
+
+	def destroy
+
+	end
+end
